4fd297057437c6441c3d993284e931c7809f6e4c
Pavel V <mental.notes@yandex.ru>
2018-02-11 13:25:16 +0300

    Добавил тесты к программе 1.4 replace

--- lab1/1.4-replace/test.bat
+++ lab1/1.4-replace/test.bat
@@ -12,11 +12,11 @@ rem При запуске с правильными параметрами ожидается нулевой код возврата
 %PROGRAM% test-data\fox.txt "%TEMP%\fox.txt" "bird" "cat"
 if ERRORLEVEL 1 goto err
 
-rem Пустой параметр входного файла
+rem При пустом параметре входного файла ожидается ненулевой код возврата
 %PROGRAM% "" "%TEMP%\fox.txt" "dog" "cat" >nul
 if NOT ERRORLEVEL 1 goto err
 
-rem Пустой параметр выходного файла
+rem При пустом параметре вЫходного файла ожидается ненулевой код возврата
 %PROGRAM% "test-data\fox.txt" "" "dog" "cat" >nul
 if NOT ERRORLEVEL 1 goto err
 
@@ -34,23 +34,53 @@ if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\fox.txt" "test-data\fox.txt" >nul
 if ERRORLEVEL 1 goto err
 
+rem Замена на пустую строку обрабатывается корректно
+%PROGRAM% "test-data\fox.txt" "%TEMP%\fox.txt" "dog" "" >nul
+if ERRORLEVEL 1 goto err
+fc.exe  "%TEMP%\fox.txt" "test-data\fox-replace-dog-with-empty-string.txt" >nul
+if ERRORLEVEL 1 goto err
+
+rem Пустое содержимое входного файла обрабатывается корректно
+%PROGRAM% "test-data\empty.txt" "%TEMP%\empty.txt" "dog" "cat" >nul
+if ERRORLEVEL 1 goto err
+fc.exe  "%TEMP%\empty.txt" "test-data\empty.txt" >nul
+if ERRORLEVEL 1 goto err
+
+rem Пустые строки во входном файле обрабатываются корректно
+%PROGRAM% "test-data\empty-lines.txt" "%TEMP%\empty-lines.txt" "dog" "cat" >nul
+if ERRORLEVEL 1 goto err
+fc.exe  "%TEMP%\empty-lines.txt" "test-data\empty-lines-out.txt" >nul
+if ERRORLEVEL 1 goto err
+
+rem Файл без искомой строки обрабатывается корректно
+%PROGRAM% "test-data\file-without-dog.txt" "%TEMP%\file-without-dog.txt" "dog" "cat" >nul
+if ERRORLEVEL 1 goto err
+fc.exe  "%TEMP%\file-without-dog.txt" "test-data\file-without-dog.txt" >nul
+if ERRORLEVEL 1 goto err
+
+rem Файл, состоящий из одной, искомой подстроки обрабатывается корректно
+%PROGRAM% "test-data\fox-only.txt" "%TEMP%\fox-only.txt" "fox" "sun" >nul
+if ERRORLEVEL 1 goto err
+fc.exe  "%TEMP%\fox-only.txt" "test-data\fox-only-out.txt" >nul
+if ERRORLEVEL 1 goto err
+
 rem Ожидается замена dog на cat с одним вхождением в строке
 %PROGRAM% "test-data\fox.txt" "%TEMP%\fox.txt" "dog" "cat"
 if ERRORLEVEL 1 goto err
 fc.exe "%TEMP%\fox.txt" "test-data\fox-replace-dog-with-cat.txt" >nul
 if ERRORLEVEL 1 goto err
 
-
-rem Многократное вхождением искомой строки в строку-заменитель обрабатывается корректно
+rem Многократные вхожения искомой строки обрабатывается корректно
+%PROGRAM% "test-data\fox-multiply.txt" "%TEMP%\fox-multiply.txt" "fox" "foxfox" >nul
+if ERRORLEVEL 1 goto err
+fc.exe  "%TEMP%\fox-multiply.txt" "test-data\fox-multiply-out.txt" >nul
+if ERRORLEVEL 1 goto err
 
 rem Замена подстроки «1231234» внутри текста «12312312345» обрабатывается корректно
-
-
-
-
-
-
-
+%PROGRAM% "test-data\file-with-number.txt" "%TEMP%\file-with-number.txt" "1231234" "fox" >nul
+if ERRORLEVEL 1 goto err
+fc.exe  "%TEMP%\file-with-number.txt" "test-data\file-with-number-out.txt" >nul
+if ERRORLEVEL 1 goto err
 
 echo OK
 exit 0
