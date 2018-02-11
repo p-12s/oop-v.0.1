a762bace05c493ce66ee7d38ed396240551b2321
Pavel V <mental.notes@yandex.ru>
2018-02-11 15:11:40 +0300

    Добавил программу Radix с тестом на кол-во входных параметров

---
+++ lab1/2.6-radix/2.6-radix.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if (argc != 4)
+	{
+		cout << "Invalid arguments count\n"
+			<< "Usage: radix.exe <source notation> <destination notation> <value>\n";
+		return 1;
+	}
+
+    return 0;
+}
+
