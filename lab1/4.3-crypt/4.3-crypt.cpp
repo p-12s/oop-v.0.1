e361cb2b476b22feb79cd05846edaa51adc1b2c0
Pavel V <mental.notes@yandex.ru>
2018-03-30 17:45:58 +0300

    Добавил шифрование/дешифрование, сделал логическую группировку кода

--- lab1/4.3-crypt/4.3-crypt.cpp
+++ lab1/4.3-crypt/4.3-crypt.cpp
@@ -20,58 +20,94 @@ int ReadNumberFromString(const string& notation)
 	}
 	return number;
 }
-/*
-ifstream OpenFileInBinaryModeForReading(const string& fileName)
+
+uint8_t CryptByte(uint8_t& byte)
 {
-	ifstream strm(fileName, ios_base::binary);
-	if (!strm.is_open())
-		cout << "Failed to open " << fileName << "\n";
-	return move(strm);
+	uint8_t bit0 = (byte & (1 << 0)) << 2;
+	uint8_t bit1 = (byte & (1 << 1)) << 2;
+	uint8_t bit2 = (byte & (1 << 2)) << 2;
+	uint8_t bit3 = (byte & (1 << 3)) << 3;
+	uint8_t bit4 = (byte & (1 << 4)) << 3;
+	uint8_t bit5 = (byte & (1 << 5)) >> 5;
+	uint8_t bit6 = (byte & (1 << 6)) >> 5;
+	uint8_t bit7 = (byte & (1 << 7)) >> 2;
+	return (bit7 | bit6 | bit5 | bit4 | bit3 | bit2 | bit1 | bit0);
 }
 
-ofstream OpenFileInBinaryModeForWriting(const string& fileName)
+uint8_t DecryptByte(const uint8_t& byte)
 {
-	ofstream strm(fileName, ios_base::binary);
-	if (!strm.is_open())
-		cout << "Failed to open " << fileName << "\n";
-	return move(strm);
-}*/
+	uint8_t bit0 = (byte & (1 << 0)) << 5;
+	uint8_t bit1 = (byte & (1 << 1)) << 5;
+	uint8_t bit2 = (byte & (1 << 2)) >> 2;
+	uint8_t bit3 = (byte & (1 << 3)) >> 2;
+	uint8_t bit4 = (byte & (1 << 4)) >> 2;
+	uint8_t bit5 = (byte & (1 << 5)) << 2;
+	uint8_t bit6 = (byte & (1 << 6)) >> 3;
+	uint8_t bit7 = (byte & (1 << 7)) >> 3;
+	return (bit7 | bit6 | bit5 | bit4 | bit3 | bit2 | bit1 | bit0);
+}
 
-void CopyFileWithBytePermutation(istream& input, ostream& output)
+bool IsFileProcessed(istream& input, ostream& output, int key, ModeOfOperation mode)
 {
-	// TODO почему тут так, и можно ли по-другому
 	uint8_t byte;
-	
 	while (input.read(reinterpret_cast<char*>(&byte), 1))
 	{
-		output << byte;
+		if (mode == ModeOfOperation::Crypt)
+		{
+			byte = byte ^ key;
+			byte = CryptByte(byte);
+			if (!output.write(reinterpret_cast<char*>(&byte), 1))
+				return false;
+		}
+		else if (mode == ModeOfOperation::Decrypt)
+		{
+			byte = DecryptByte(byte);			
+			byte = byte ^ key;
+			if (!output.write(reinterpret_cast<char*>(&byte), 1))
+				return false;
+		}
+		else
+			return false;
 	}
+	return true;
 }
 
-uint8_t EncodeByte(const uint8_t& byte)
+ModeOfOperation GetModeParamether(const string& modeArgc)
 {
-	uint8_t bit0 = (byte & 1) << 2;
-	uint8_t bit1 = (byte & 1 << 1) << 2;
-	uint8_t bit2 = (byte & 1 << 2) << 2;
-	uint8_t bit3 = (byte & 1 << 3) << 3;
-	uint8_t bit4 = (byte & 1 << 4) << 3;
-	uint8_t bit5 = (byte & 1 << 5) >> 5;
-	uint8_t bit6 = (byte & 1 << 6) >> 5;
-	uint8_t bit7 = (byte & 1 << 7) >> 2;
-	return bit7 | bit6 | bit5 | bit4 | bit3 | bit2 | bit1 | bit0;
+	ModeOfOperation mode =	(modeArgc == "crypt") ? ModeOfOperation::Crypt :
+							(modeArgc == "decrypt") ? ModeOfOperation::Decrypt :
+							ModeOfOperation::Unknown;
+	if (mode == ModeOfOperation::Unknown)
+		throw runtime_error("Invalid encryption mode. Use crypt or encrypt\n");
+	return mode;
 }
 
-uint8_t DecodeByte(const uint8_t& byte)
+int GetKeyParamether(const string& modeArgc)
 {
-	uint8_t bit0 = (byte & 1) << 5;
-	uint8_t bit1 = (byte & 1 << 1) << 5;
-	uint8_t bit2 = (byte & 1 << 2) >> 2;
-	uint8_t bit3 = (byte & 1 << 3) >> 2;
-	uint8_t bit4 = (byte & 1 << 4) >> 2;
-	uint8_t bit5 = (byte & 1 << 5) << 2;
-	uint8_t bit6 = (byte & 1 << 6) >> 3;
-	uint8_t bit7 = (byte & 1 << 7) >> 3;
-	return bit7 | bit6 | bit5 | bit4 | bit3 | bit2 | bit1 | bit0;
+	int key = ReadNumberFromString(modeArgc);
+	if (key < 0 || key > 255)
+		throw invalid_argument("Paramether key must be in interval [0, 255]\n");
+	return key;
+}
+
+void CopyFileWithEncryption(char* argv[])
+{
+	ModeOfOperation mode = GetModeParamether(argv[1]);
+	int key = GetKeyParamether(argv[4]);
+
+	ifstream inputFile(argv[2]);
+	if (!inputFile.is_open())
+		throw runtime_error("Failed to open file for reading\n");
+
+	ofstream outputFile(argv[3]);
+	if (!outputFile.is_open())
+		throw runtime_error("Failed to open file for writing\n");
+
+	if (!IsFileProcessed(inputFile, outputFile, key, mode))
+		throw runtime_error("An error occurred while processing the file\n");
+
+	if (!outputFile.flush())
+		throw runtime_error("Failed to save data on disk\n");
 }
 
 int main(int argc, char* argv[])
@@ -94,57 +130,15 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	// проверка режима
-	// возможно она излишня
-
-	string modeParamether = argv[1];
-	ModeOfOperation mode = (modeParamether == "crypt") ? ModeOfOperation::Crypt :
-							(modeParamether == "decrypt") ? ModeOfOperation::Decrypt :
-							ModeOfOperation::Unknown;
-	
-	if (mode == ModeOfOperation::Unknown)
-	{
-		cout << "Invalid encryption mode!";
-		return 1;
-	}
-
-	// проверка параметра шифрования key	
 	try
 	{
-		int key = ReadNumberFromString(argv[4]);
-
-		if (key < 0 || key > 255)
-			throw invalid_argument("Paramether key must be in interval [0, 255]\n");
-
-		cout << key << endl;
+		CopyFileWithEncryption(argv);
 	}
 	catch (const exception& error)
 	{
 		cout << error.what();
 		return 1;
 	}
-
-	ifstream inputFile(argv[2]);
-	if (!inputFile.is_open())
-	{
-		cout << "Failed to open " << argv[2] << " for reading" << endl;
-		return 1;
-	}
-	ofstream outputFile(argv[3]);
-	if (!outputFile.is_open())
-	{
-		cout << "Failed to open " << argv[3] << " for writing" << endl;
-		return 1;
-	}
-
-	CopyFileWithBytePermutation(inputFile, outputFile);
-
-	if (!outputFile.flush())
-	{
-		cout << "Failed to save data on disk\n";
-		return 1;
-	}
-
 
     return 0;
 }
