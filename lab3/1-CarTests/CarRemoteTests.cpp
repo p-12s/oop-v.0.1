46beb77814066ecb3a4ab4d08c3ab775d5af3291
Pavel V <mental.notes@yandex.ru>
2018-04-01 15:27:20 +0300

    Исправил некомпиляцию проекта

--- lab3/1-CarTests/CarRemoteTests.cpp
+++ lab3/1-CarTests/CarRemoteTests.cpp
@@ -6,10 +6,10 @@ using namespace std;
 BOOST_AUTO_TEST_SUITE(CarRemote_function)
 
 	// Тестовый тест
-	BOOST_AUTO_TEST_CASE(empty_vector_equal_himself)
+	/*BOOST_AUTO_TEST_CASE(empty_vector_equal_himself)
 	{
 		vector<double> emptyVector;
 		BOOST_CHECK(emptyVector.empty());
-	}
+	}*/
 
 BOOST_AUTO_TEST_SUITE_END()
--- lab3/1-CarTests/stdafx.h
+++ lab3/1-CarTests/stdafx.h
@@ -1,3 +1,7 @@
 #pragma once
 
 #include "targetver.h"
+#include <boost/algorithm/string/replace.hpp>
+#define BOOST_TEST_INCLUDED
+#pragma warning (disable: 4702)
+#include <boost/test/unit_test.hpp>
