fa6292bdef2bc8d93f012ab546f3705339a5a39f
pavel.vedernikov <pavel.vedernikov@travelline.ru>
2018-04-26 01:13:11 +0300

    Revert "Добавил основу тестов в 1.2-Shape"
    This reverts commit e3248254381e1cce70e296bb664b5f62f976863d.

--- lab4/1.2-Shape/stdafx.h
+++ lab4/1.2-Shape/stdafx.h
@@ -1,12 +1,6 @@
 #pragma once
 
 #include "targetver.h"
-
 #include <stdio.h>
 #include <tchar.h>
-#include <iostream>
 #include <string>
-#include <sstream>
-#include <functional>
-#include <map>
-#include <vector>
