3903ccc8fd43e9a9cc44b7f95cc059f2d3f65e19
Pavel V <mental.notes@yandex.ru>
2018-05-01 18:47:21 +0300

    Добавил директиву в заголовочный файл

--- lab5/1.1-ComplexTests/stdafx.h
+++ lab5/1.1-ComplexTests/stdafx.h
@@ -5,6 +5,7 @@
 #define BOOST_TEST_INCLUDED
 #pragma warning (disable: 4702)
 #include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
 
 #include <stdio.h>
 #include <tchar.h>
