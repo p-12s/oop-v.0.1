7950a69aed23fa70bdfc682c10df98d32e504a87
Pavel V <mental.notes@yandex.ru>
2018-05-12 23:47:13 +0300

    Добавил проект lab7 с настроенными boost-тестами

---
+++ lab7/1.3-FindMaxEx/1.3-FindMaxEx.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+using namespace std;
+
+int main()
+{
+	cout << 222 << endl;
+
+    return 0;
+}
+
---
+++ lab7/1.3-FindMaxExTests/FindMaxExTests.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+
+using namespace std;
+
+struct FindMaxExFixture
+{
+	FindMaxExFixture()
+	{
+	}
+};
+
+BOOST_FIXTURE_TEST_SUITE(FindMaxEx, FindMaxExFixture)
+
+BOOST_AUTO_TEST_CASE(test_test)
+{
+	BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
